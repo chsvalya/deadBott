d11c17e8f0346f8f1926f7c6e40746225674fa51
Елена Белодедова <enbelodedova@edu.hse.ru>
2019-12-08 19:04:41 +0300

    добавила классы,Bot описывает бота, BotSettings ловит поступления запросов с чатика,Command разделяет команды к нашему боту от других команд

---
+++ DeadBot/Commands/Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeadBot.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace DeadBot.Commands
+{
+    /* не знаю на сколько понадобится,но таким образом,можно сохранить пересечение
+     * команд в чатике */
+    public abstract class Command
+    {
+        public abstract string Name { get; }
+
+        public abstract void Execute(Message message, TelegramBotClient client);
+
+        public bool Contains(string command)
+        {
+            return command.Contains(this.Name) &&
+                command.Contains(BotSettings.Name);
+        }
+    }
+}
---
+++ DeadBot/Models/Bot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace DeadBot.Models
+{
+    /*инициализация бота и его команд */
+    public static class Bot
+    {
+        private static TelegramBotClient client;
+
+        public static async Task<TelegramBotClient> Get() 
+        {
+            if(client != null)
+            {
+                return client;
+            }
+            client = new TelegramBotClient(BotSettings.Key);
+            await client.SetWebhookAsync("");
+
+            return client;
+        }
+    }
+}
